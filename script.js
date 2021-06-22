7e7aa46ddbbcf0aebbf432eacf5d68ec26130742
Lewis <lewisholliday@hotmail.co.uk>
2021-06-22 19:09:19 +0100

    made game run for 5 turns and keep score

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let userScore = 0;
+
+//** Prompts computer to pick a random option from array */
 function computerPlay() {
     let options = new Array();
     options = ["rock", "paper", "scissors"];
@@ -12,12 +16,31 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") || 
         (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") || 
         (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") ){
+        userScore++;
         return "Winner!!";
     } else {
+        computerScore++;
         return "Loser!!";
     }    
 }
 
+//** Runs game to a best of 5 */
 function game() {
+    let playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+    console.log(playRound(playerSelection, computerPlay()));
+    if(userScore < 5 && computerScore < 5){
+        game();
+    }
+    else {
+        endGame()
+    }
+}
+
+function endGame() {
+    if (userScore > computerScore) {
+        console.log ("Game over! You win!")
+        } else if (computerScore > UserScore) {
+            console.log ("You lose! Better luck next time.")
+        }
     
 }
