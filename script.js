706665c4fdc93e337d756bab6b56c164103d9f67
Lewis <lewisholliday@hotmail.co.uk>
2021-07-11 16:05:10 +0100

    started to ammend JS, cashed DOM variables to work on later

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let computerScore = 0;
 let userScore = 0;
+let userScore_span = document.getElementById("user-score");
+let computerScore_span = document.getElementById("computer-score");
+let scoreBoard_div = document.querySelector(".score-board");
+let result_div = document.querySelector(".result");
+let paper_div = document.getElementById("paper");
+let scissors_div = document.getElementById("scissors");
+
+window.onload = function() {
+    let rock_btn = document.getElementById("rock");
+    rock_btn.addEventListener("click",function(){
+        console.log("clicked")
+    });
+}
+
 
 //** Prompts computer to pick a random option from array */
 function computerPlay() {
